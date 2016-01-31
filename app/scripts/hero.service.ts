04aab07a6c50f25106eb3a1d7ae8a31791b15b23
sookool99 <shawnmacburnie@gmail.com>
2016-01-31 19:11:19 -0400

    followed angular2's demos

---
+++ app/scripts/hero.service.ts
@@ -0,0 +1,13 @@
+import {HEROES} from './mock-heroes';
+import {Injectable} from 'angular2/core';
+import {Hero} from './hero'
+@Injectable()
+export class HeroService {
+	getHeroes() {
+		return Promise.resolve(HEROES);
+	}
+	getHeroesSlowly() {
+		return new Promise<Hero[]>(resolve =>
+			setTimeout(() => resolve(HEROES), 2000)) //simulate server delay
+	}
+}
